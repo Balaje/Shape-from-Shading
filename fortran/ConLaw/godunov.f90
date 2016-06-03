059de1d12e78196b80f34672921ed259e29be60a
Balaje <balaje6@gmail.com>
2016-06-03 13:56:29 +0530

    Modified godunov.f90

--- fortran/ConLaw/godunov.f90
+++ fortran/ConLaw/godunov.f90
@@ -9,8 +9,8 @@ program godunov
   use functions, only : f
   use flux, only : fgod
 
-  integer, parameter :: N = 100
-  real(kind = 8) :: a = -5, b = 5, h, delt, tf, t0, r, lt = 1., rt = 0.
+  integer, parameter :: N = 500
+  real(kind = 8) :: a = 0., b = 1., h, delt, tf, t0, r, lt = 1., rt = 0.
   real(kind = 8), dimension(N-1) :: v,vnew,x
   integer :: M, i, j
 
@@ -19,21 +19,16 @@ program godunov
   h = (b-a)/N
 
   ! CFL Condition
-  delt = 0.8*h
+  delt = 1*h
 
   ! Defining the mesh and the initial condition
+  pi = acos(-1.)
   do i = 1,N-1
     x(i) = a + i*h
-    if(x(i) < 0) then
-      v(i) = 1.
-    elseif(x(i) >= 0 .and. x(i) <= 1) then
-      v(i) = 2*x(i)**3 - 3*x(i)**2 + 1
-    elseif(x(i) > 1) then
-      v(i) = 0.
-    endif
+    v(i) = sin(pi*x(i))
   enddo
 
-  tf = 1.
+  tf = 0.2
   t0 = 0.
 
   M = (tf-t0)/delt
@@ -41,11 +36,11 @@ program godunov
   r = delt/h
 
   do j = 1,M
-    vnew(1) = v(1) - r*(fgod(f,v(2),v(1)) - fgod(f,v(1),lt))
+    vnew(1) = v(1) - r*(fgod(f,v(2),v(1)) - fgod(f,v(1),sin(pi*a)))
     do i = 2,N-2
       vnew(i) = v(i) - r*(fgod(f,v(j+1),v(j)) - fgod(f,v(j),v(j-1)))
     enddo
-    vnew(N-1) = v(N-1) - r*(fgod(f,rt,v(N-1)) - fgod(f,v(N-1),v(N-2)))
+    vnew(N-1) = v(N-1) - r*(fgod(f,sin(pi*b),v(N-1)) - fgod(f,v(N-1),v(N-2)))
 
     v = vnew
   enddo
