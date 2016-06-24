b3214df9a15a2432544635a9cdaf9c84030060c8
Balaje <balaje6@gmail.com>
2016-06-24 14:48:33 +0530

    Added files for 1d orthographic projection

--- MATLAB/SFS/J.m
+++ MATLAB/SFS/J.m
@@ -1,3 +1,4 @@
 function V = J(x)
-    V = 1/sqrt(1+(1-2*x)^2);
+    V = 1/sqrt(1+(pi*cos(pi*x))^2);
+    %V = 1/sqrt(1+((1-2*x))^2);
 end
--- MATLAB/SFS/upwind.m
+++ MATLAB/SFS/upwind.m
@@ -12,49 +12,78 @@ close all
 format long
 
 a = 0; b = 1;
-N = 50;
+N = 10;
 
 global upx
 
-h = (b-a)/N;
-
-x = (a+h:h:b-h)';
-
-u = zeros(N-1,1);
-unew = zeros(N-1,1);
-
-delt = h;
-
-error = 100;
-tol = 1e-5;
-
-while error > tol
-    % i = 1
-    Dm = (u(1))/h;
-    Dp = (u(2)-u(1))/h;
-    unew(1) = u(1) - delt*(godunov(Dm,Dp,x(1),x(2)) - sqrt(1-J(x(1))^2));
+for p=1:4
+    h(p) = (b-a)/N;
     
-    % i = 2:N-2
-    for i = 2:N-2
-        if(J(x(i)) == 1)
-             unew(i) = 1/4;
-         else
-            Dm = (u(i)-u(i-1))/h;
-            Dp = (u(i+1)-u(i))/h;
-            unew(i) = u(i) - delt*(godunov(Dm,Dp,x(i),x(i+1)) - sqrt(1-J(x(i))^2));
+    x = (a+h(p):h(p):b-h(p))';
+    
+    u = zeros(N-1,1);
+    for i=1:N-1
+        u(i) = 0;
+    end
+    unew = zeros(N-1,1);
+    
+    delt = h(p);
+    
+    error = 100;
+    tol = 1e-5;
+    
+    while error > tol
+        % i = 1
+        Dm = (u(1))/h(p);
+        Dp = (u(2)-u(1))/h(p);
+        unew(1) = u(1) - delt*(godunov(Dm,Dp,a,x(2)) - sqrt(1-J(x(1))^2));
+        
+        % i = 2:N-2
+        for i = 2:N-2
+            if(J(x(i)) == 1)
+                %             if(x(i)==0.25)
+                %                 unew(i) = 1;
+                %             elseif(x(i) == 0.75)
+                %                 unew(i) = -1;
+                %             end
+                unew(i) = 1;
+            else
+                Dm = (u(i)-u(i-1))/h(p);
+                Dp = (u(i+1)-u(i))/h(p);
+                unew(i) = u(i) - delt*(godunov(Dm,Dp,x(i-1),x(i+1)) - sqrt(1-J(x(i))^2));
+            end
         end
+        
+        % i = N-1
+        Dm = (u(N-1)-u(N-2))/h(p);
+        Dp = (-u(N-1))/h(p);
+        unew(N-1) = u(N-1) - delt*(godunov(Dm,Dp,x(N-2),b) - sqrt(1-J(x(N-1))^2));
+        
+        error = max(abs(u-unew));
+        u = unew;
+        
     end
     
-    % i = N-1
-    Dm = (u(N-1)-u(N-2))/h;
-    Dp = (-u(N-1))/h;
-    unew(N-1) = u(N-1) - delt*(godunov(Dm,Dp,x(N-1),b) - sqrt(1-J(x(N-1))^2));
+    exact = zeros(N-1,1);
+    for i=1:N-1
+        exact(i) = sin(pi*x(i));
+        %exact(i) = x(i)*(1-x(i));
+    end
     
-    error = max(abs(u-unew));
-    u = unew;
+    figure(p);
+    plot(x,unew,x,exact,'*');
+    grid on
     
+    error1(p) = max(abs(unew(i)-exact(i)));
+    
+    N = N*2;
 end
 
-plot(x,unew);
+for j=1:p-1
+    order(j) = log(error1(j)/error1(j+1))/log(2.);
+end
+
+%order'
+error1'
 
 
