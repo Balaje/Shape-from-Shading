128407833856b17913f050b02c8bd732437849e1
Balaje <balaje6@gmail.com>
2016-06-28 23:03:20 +0530

    Changes

--- fortran/sfs/Perspective/persp_prados.f90
+++ fortran/sfs/Perspective/persp_prados.f90
@@ -4,27 +4,32 @@
 program persp1
 implicit none
 
-integer, parameter :: N = 200
+integer, parameter :: Nx = 200, Ny = 200
 real(kind = 8), parameter :: a = 0., b = 1., c = 0., d = 1., tol = 1e-8, f = 1., pi = acos(-1.)
-real(kind = 8), dimension(N+1,N+1) :: u, unew, I, exact, M, Q
-real(kind = 8), dimension(N+1) :: x,y
+real(kind = 8), dimension(Nx+1,Ny+1) :: u, unew, I, exact, M, Q
+real(kind = 8), dimension(Nx+1) :: x
+real(kind = 8), dimension(Ny+1) :: y
 real(kind = 8), dimension(3) :: Ax, Ay
-real(kind = 8) :: h, delt, error, Dxp, Dxm, Dyp, Dym, Dx, Dy, L, error1
+real(kind = 8) :: hx, hy, delt, error, Dxp, Dxm, Dyp, Dym, Dx, Dy, L, error1
 integer :: j,k,Ix,Iy,p
 
-h = (b-a)/N;
+hx = (b-a)/Nx
+hy = (d-c)/Ny
 
-do j = 1,N+1
-   x(j) = a + (j-1)*h
-   y(j) = c + (j-1)*h
+do j = 1,Nx+1
+   x(j) = a + (j-1)*hx
 enddo
 
-open(unit = 1, file = "thing2.txt")
+do j = 1,Ny+1
+   y(j) = c + (j-1)*hy
+enddo
+
+open(unit = 1, file = "moz.txt")
 read(1, *) I
 
 ! Defining Parameters
-do k = 1,N+1
-   do j = 1,N+1
+do k = 1,Ny+1
+   do j = 1,Nx+1
       u(j,k) = 0.
       if(I(j,k) < 0.0001) then
          I(j,k) = 0.01
@@ -35,15 +40,15 @@ do k = 1,N+1
    enddo
 enddo
 
-delt = 0.5*h
+delt = 0.5*min(hx,hy)
 error = 100.
 
 !  do p = 1,1000
 do while (error> tol)
    !j = 1, k = 1
-   Dxp = (u(2,1)-u(1,1))/h
+   Dxp = (u(2,1)-u(1,1))/hx
    Dxm = 0.
-   Dyp = (u(1,2)-u(1,1))/h
+   Dyp = (u(1,2)-u(1,1))/hy
    Dym = 0.
 
    Ax = (/0.,Dxp,Dxm/)
@@ -58,33 +63,33 @@ do while (error> tol)
    unew(1,1) = u(1,1) - delt*L
 
    ! j = 1, k = N+1
-   Dxp = (u(2,N+1)-u(1,N+1))/h
+   Dxp = (u(2,Ny+1)-u(1,Ny+1))/hx
    Dxm = 0.
    Dyp = 0.
-   Dym = (u(1,N+1)-u(1,N))/h
+   Dym = (u(1,Ny+1)-u(1,Ny))/hy
 
    Ax = (/0.,Dxp,Dxm/)
    Ay = (/0.,Dyp,Dym/)
 
-   Dx = max(0.,-M(2,N+1)*Dxp,M(1,N+1)*Dxm)
-   Dy = max(0.,-M(1,N+1)*Dyp,M(1,N)*Dym)
+   Dx = max(0.,-M(2,Ny+1)*Dxp,M(1,Ny+1)*Dxm)
+   Dy = max(0.,-M(1,Ny+1)*Dyp,M(1,Ny)*Dym)
    Ix = maxloc((/0.,-Dxp,Dxm/),dim=1)
    Iy = maxloc((/0.,-Dyp,Dym/),dim=1)
 
-   L = -exp(-2*u(1,N+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(1,N+1)**2*(Ax(Ix)*x(1)+Ay(Iy)*y(N+1))**2 + M(1,N+1)**2*Q(1,N+1)**2)
-   unew(1,N+1) = u(1,N+1) - delt*L
+   L = -exp(-2*u(1,Ny+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(1,Ny+1)**2*(Ax(Ix)*x(1)+Ay(Iy)*y(Ny+1))**2 + M(1,Ny+1)**2*Q(1,Ny+1)**2)
+   unew(1,Ny+1) = u(1,Ny+1) - delt*L
 
-   do k = 2,N
-      do j = 2,N
+   do k = 2,Ny
+      do j = 2,Nx
          if(I(j,k) == 0.) then
             unew(j,k) = (u(j-1,k)+u(j+1,k)+u(j,k-1)+u(j,k+1))/4
          elseif(I(j,k) == 1.)then
             unew(j,k) = 0.
          else
-            Dxp = (u(j+1,k)-u(j,k))/h
-            Dxm = (u(j,k)-u(j-1,k))/h
-            Dyp = (u(j,k+1)-u(j,k))/h
-            Dym = (u(j,k)-u(j,k-1))/h
+            Dxp = (u(j+1,k)-u(j,k))/hx
+            Dxm = (u(j,k)-u(j-1,k))/hx
+            Dyp = (u(j,k+1)-u(j,k))/hy
+            Dym = (u(j,k)-u(j,k-1))/hy
 
             Ax = (/0.,Dxp,Dxm/)
             Ay = (/0.,Dyp,Dym/)
@@ -102,43 +107,43 @@ do while (error> tol)
 
    ! j = N+1, k = 1
    Dxp = 0.
-   Dxm = (u(N+1,1)-u(N,1))/h
-   Dyp = (u(N+1,2)-u(N+1,1))/h
+   Dxm = (u(Nx+1,1)-u(Nx,1))/hx
+   Dyp = (u(Nx+1,2)-u(Nx+1,1))/hy
    Dym = 0.
 
    Ax = (/0.,Dxp,Dxm/)
    Ay = (/0.,Dyp,Dym/)
 
-   Dx = max(0.,-M(N+1,1)*Dxp,M(N,1)*Dxm)
-   Dy = max(0.,-M(N+1,2)*Dyp,M(N+1,1)*Dym)
+   Dx = max(0.,-M(Nx+1,1)*Dxp,M(Nx,1)*Dxm)
+   Dy = max(0.,-M(Nx+1,2)*Dyp,M(Nx+1,1)*Dym)
    Ix = maxloc((/0.,-Dxp,Dxm/),dim=1)
    Iy = maxloc((/0.,-Dyp,Dym/),dim=1)
 
-   L = -exp(-2*u(N+1,1)) + sqrt(f**2*(Dx**2+Dy**2) + M(N+1,1)**2*(Ax(Ix)*x(N+1)+Ay(Iy)*y(1))**2 + (M(N+1,1)*Q(N+1,1))**2 )
-   unew(N+1,1) = u(N+1,1) - delt*L
+   L = -exp(-2*u(Nx+1,1)) + sqrt(f**2*(Dx**2+Dy**2) + M(Nx+1,1)**2*(Ax(Ix)*x(Nx+1)+Ay(Iy)*y(1))**2 + (M(Nx+1,1)*Q(Nx+1,1))**2 )
+   unew(Nx+1,1) = u(Nx+1,1) - delt*L
 
    ! j = N+1, k = N+1
    Dxp = 0.
-   Dxm = (u(N+1,N+1)-u(N,N+1))/h
+   Dxm = (u(Nx+1,Ny+1)-u(Nx,Ny+1))/hx
    Dyp = 0.
-   Dym = (u(N+1,N+1)-u(N+1,N))/h
+   Dym = (u(Nx+1,Ny+1)-u(Nx+1,Ny))/hy
 
    Ax = (/0.,Dxp,Dxm/)
    Ay = (/0.,Dyp,Dym/)
 
-   Dx = max(0.,-M(N+1,N+1)*Dxp,M(N,N+1)*Dxm)
-   Dy = max(0.,-M(N+1,N+1)*Dyp,M(N+1,N)*Dym)
+   Dx = max(0.,-M(Nx+1,Ny+1)*Dxp,M(Nx,Ny+1)*Dxm)
+   Dy = max(0.,-M(Nx+1,Ny+1)*Dyp,M(Nx+1,Ny)*Dym)
    Ix = maxloc((/0.,-Dxp,Dxm/),dim=1)
    Iy = maxloc((/0.,-Dyp,Dym/),dim=1)
 
-   L = -exp(-2*u(N+1,N+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(N+1,N+1)**2*(Ax(Ix)*x(N+1)+Ay(Iy)*y(N+1))**2 + (M(N+1,N+1)*Q(N+1,N+1))**2 )
-   unew(N+1,N+1) = u(N+1,N+1) - delt*L
+   L = -exp(-2*u(Nx+1,Ny+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(Nx+1,Ny+1)**2*(Ax(Ix)*x(Nx+1)+Ay(Iy)*y(Ny+1))**2 + (M(Nx+1,Ny+1)*Q(Nx+1,Ny+1))**2 )
+   unew(Nx+1,Ny+1) = u(Nx+1,Ny+1) - delt*L
 
    !j = 2:N, k = 1
-   do j=2,N
-      Dxp = (u(j+1,1)-u(j,1))/h
-      Dxm = (u(j,1)-u(j-1,1))/h
-      Dyp = (u(j,2)-u(j,1))/h
+   do j=2,Nx
+      Dxp = (u(j+1,1)-u(j,1))/hx
+      Dxm = (u(j,1)-u(j-1,1))/hx
+      Dyp = (u(j,2)-u(j,1))/hy
       Dym = 0
 
       Ax = (/0.,Dxp,Dxm/)
@@ -154,30 +159,30 @@ do while (error> tol)
    enddo
 
    !j = 2:N,k=N+1
-   do j=2,N
-      Dxp = (u(j+1,N+1)-u(j,N+1))/h
-      Dxm = (u(j,N+1)-u(j-1,N+1))/h
+   do j=2,Nx
+      Dxp = (u(j+1,Ny+1)-u(j,Ny+1))/hx
+      Dxm = (u(j,Ny+1)-u(j-1,Ny+1))/hx
       Dyp = 0.
-      Dym = (u(j,N+1)-u(j,N))/h
+      Dym = (u(j,Ny+1)-u(j,Ny))/hy
 
       Ax = (/0.,Dxp,Dxm/)
       Ay = (/0.,Dyp,Dym/)
 
-      Dx = max(0.,-M(j+1,N+1)*Dxp,M(j-1,N+1)*Dxm)
-      Dy = max(0.,-M(j,N+1)*Dyp,M(j,N)*Dym)
+      Dx = max(0.,-M(j+1,Ny+1)*Dxp,M(j-1,Ny+1)*Dxm)
+      Dy = max(0.,-M(j,Ny+1)*Dyp,M(j,Ny)*Dym)
       Ix = maxloc((/0.,-Dxp,Dxm/),dim=1)
       Iy = maxloc((/0.,-Dyp,Dym/),dim=1)
 
-      L = -exp(-2*u(j,N+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(j,N+1)**2*(Ax(Ix)*x(j)+Ay(Iy)*y(N+1))**2 + (M(j,N+1)*Q(j,N+1))**2 )
-      unew(j,N+1) = u(j,N+1) - delt*L
+      L = -exp(-2*u(j,Ny+1)) + sqrt(f**2*(Dx**2+Dy**2) + M(j,Ny+1)**2*(Ax(Ix)*x(j)+Ay(Iy)*y(Ny+1))**2 + (M(j,Ny+1)*Q(j,Ny+1))**2 )
+      unew(j,Ny+1) = u(j,Ny+1) - delt*L
    enddo
 
    !k = 2:N, j = 1
-   do k = 2,N
-      Dxp = (u(2,k)-u(1,k))/h
+   do k = 2,Ny
+      Dxp = (u(2,k)-u(1,k))/hx
       Dxm = 0.
-      Dyp = (u(1,k+1)-u(1,k))/h
-      Dym = (u(1,k)-u(1,k-1))/h
+      Dyp = (u(1,k+1)-u(1,k))/hy
+      Dym = (u(1,k)-u(1,k-1))/hy
 
       Ax = (/0.,Dxp,Dxm/)
       Ay = (/0.,Dyp,Dym/)
@@ -192,35 +197,35 @@ do while (error> tol)
    enddo
 
    !k=2:N, j = N+1
-   do k=2,N
+   do k=2,Nx
       Dxp = 0.
-      Dxm = (u(N+1,k)-u(N,k))/h
-      Dyp = (u(N+1,k+1)-u(N+1,k))/h
-      Dym = (u(N+1,k)-u(N+1,k-1))/h
+      Dxm = (u(Nx+1,k)-u(Nx,k))/hx
+      Dyp = (u(Nx+1,k+1)-u(Nx+1,k))/hy
+      Dym = (u(Nx+1,k)-u(Nx+1,k-1))/hy
 
       Ax = (/0.,Dxp,Dxm/)
       Ay = (/0.,Dyp,Dym/)
 
-      Dx = max(0.,-M(N+1,k)*Dxp,M(N,k)*Dxm)
-      Dy = max(0.,-M(N+1,k+1)*Dyp,M(N+1,k-1)*Dym)
+      Dx = max(0.,-M(Nx+1,k)*Dxp,M(Nx,k)*Dxm)
+      Dy = max(0.,-M(Nx+1,k+1)*Dyp,M(Nx+1,k-1)*Dym)
       Ix = maxloc((/0.,-Dxp,Dxm/),dim=1)
       Iy = maxloc((/0.,-Dyp,Dym/),dim=1)
 
-      L = -exp(-2*u(N+1,k)) + sqrt(f**2*(Dx**2+Dy**2) + M(N+1,k)**2*(Ax(Ix)*x(N+1)+Ay(Iy)*y(k))**2 + (M(N+1,k)*Q(N+1,k))**2 ) 
-      unew(N+1,k) = u(N+1,k) - delt*L
+      L = -exp(-2*u(Nx+1,k)) + sqrt(f**2*(Dx**2+Dy**2) + M(Nx+1,k)**2*(Ax(Ix)*x(Nx+1)+Ay(Iy)*y(k))**2 + (M(Nx+1,k)*Q(Nx+1,k))**2 )
+      unew(Nx+1,k) = u(Nx+1,k) - delt*L
    enddo
 
 
    error = maxval(abs(u-unew))
-  print 11, error
+ print 11, error
 11   format("error=", se22.15)
 
    u = unew
 enddo
 
 open(unit = 1,file = "op_neumannf.txt")
-do j=1,N+1
-   do k=1,N+1
+do j=1,Nx+1
+   do k=1,Ny+1
       write(1,*) x(j), y(k),-exp(unew(j,k))
    enddo
 enddo
--- fortran/sfs/Perspective/plot2.m
+++ fortran/sfs/Perspective/plot2.m
@@ -1,6 +1,7 @@
 load op_neumannf.txt
 Z1 = op_neumannf(:,3);
 z2 = reshape(Z1,[201,201]);
-x = 0:1/200:1;
-y = 0:1/200:1;
-surf(x,y,z2,'EdgeColor','black')
+x = linspace(0,1,201);
+y = linspace(0,1,201);
+surf(x,y,z2','EdgeColor','black')
+axis([0,1,0,1,-1.2,-0.4])
